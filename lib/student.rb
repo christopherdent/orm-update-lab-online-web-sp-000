65c02bf3b13f1f9d251195e93cd29927e5a1786e
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:51:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,11 @@ require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
+ 
   attr_accessor :name, :grade, :id 
   
-  
+ 
+
   def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
@@ -13,7 +15,7 @@ class Student
 
 
   def self.create_table
-    
+
     sql = <<-SQL 
     
     CREATE TABLE students (
@@ -32,12 +34,12 @@ class Student
     SQL
     DB[:conn].execute(sql)  
   end 
- 
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
- 
+
   def save 
    if self.id 
     self.update 
@@ -47,7 +49,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade) 
-   
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
    end 
   end 
@@ -57,19 +59,19 @@ class Student
     student.save
   end 
 
-   
-    
+
+
   def self.new_from_db(row)
-   
+
     new_student = Student.new(row[0], row[1], row[2])   
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
     new_student 
-    
+binding.pry 
   end
 
-    
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students 
@@ -78,11 +80,6 @@ class Student
     SQL
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first   
   end 
-
-
-
-
-
-
-
-end
+  
+  
+end 
